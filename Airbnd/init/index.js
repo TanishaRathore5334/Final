8a010de08304c78a0da308dfc4d02128e6cbaa80
Tanisha Rathore <tumharaemail@example.com>
2025-08-24 19:57:58 +0530

    Initial commit

---
+++ Airbnd/init/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const initdata = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+  .then(() => {
+    console.log("connect to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+const initDB = async () => {
+  await Listing.deleteMany({});
+ initdata.data= initdata.data.map((obj)=>({...obj,owner:"6876252b5a757df18897aa7b"}));
+   await Listing.insertMany(initdata.data);
+  console.log("data was initialize");
+};
+initDB();
