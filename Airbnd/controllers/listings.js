5d415affaaab2e5b3041891ae28aba5d8f613d90
TanishaRathore5334 <tanisharathore011@gmail.com>
2025-11-27 12:37:23 +0530

    Update listings.js

--- Airbnd/controllers/listings.js
+++ Airbnd/controllers/listings.js
@@ -4,11 +4,9 @@ module.exports.index=async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
   }
-
   module.exports.renderNewForm=(req, res) => {
   res.render("listings/new.ejs");
 }
-
 module.exports.showListing=async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id)
